516e5948b1e30b6371e88544e1f7d8b3889a4930
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-08-02 13:45:49 +0100

    created TestimonialCard

---
+++ resources/js/components/partials/home/FifthSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+         <TestimonialCard>
+            <template #img>
+                <img src="/assets/images/json-wong.webp" alt="" srcset="">
+            </template>
+            <template #name>
+                Jason Wong
+            </template>
+            <template #role>
+                CEO at Omnicharge
+            </template>
+            <template #testimony>
+                The work of the Belkins team was very productive. We were especially impressed by the deep research - it seemed that Belkins experts started speaking our brand’s language after delving into our style guidelines and investigating them for a while.
+            </template>
+        </TestimonialCard>
+    </section>
+</template>
+<script>
+import TestimonialCard  from '../../UI/TestimonialCard.vue'
+export default {
+    components:{
+        TestimonialCard
+    }
+}
+</script>
--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="bg-gray-100">
         <FirstSection />
+       <FifthSection />
         <!-- <p class="">This is my HOME PAGE component content.</p> -->
     </div>
 </template>
 <script>
 import FirstSection  from '../components/partials/home/FirstSection.vue'
+import FifthSection  from '../components/partials/home/FifthSection.vue'
 export default {
     components:{
-        FirstSection
+        FirstSection,FifthSection
     }
 }
 </script>
